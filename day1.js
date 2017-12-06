de418c4f761212faa5f9c97b156adcbc285b7901
Will Hughes <willhughes091@gmail.com>
2017-12-06 16:26:27 +0000

    Surround day 1 solution with IIFE for consistency

--- day1.js
+++ day1.js
@@ -1,10 +1,12 @@
-const input = document.body.textContent.trim();
-const inputDigits = input.split("").map(Number);
+(() => {
+  const input = document.body.textContent.trim();
+  const inputDigits = input.split("").map(Number);
 
-const getPairs = (values, shift) => values.map((value, index) => ({ current: value, next: values[(index + shift) % values.length] }));
-const sumIdenticalPairs = pairs => pairs.map(pair => pair.current === pair.next ? pair.current : 0).reduce((sum, pairValue) => sum + pairValue);
+  const getPairs = (values, shift) => values.map((value, index) => ({ current: value, next: values[(index + shift) % values.length] }));
+  const sumIdenticalPairs = pairs => pairs.map(pair => pair.current === pair.next ? pair.current : 0).reduce((sum, pairValue) => sum + pairValue);
 
-const part1Solution = sumIdenticalPairs(getPairs(inputDigits, 1))
-const part2Solution = sumIdenticalPairs(getPairs(inputDigits, inputDigits.length / 2))
+  const part1Solution = sumIdenticalPairs(getPairs(inputDigits, 1))
+  const part2Solution = sumIdenticalPairs(getPairs(inputDigits, inputDigits.length / 2))
 
-console.log("Part 1 Solution:", part1Solution, "\nPart 2 Solution:", part2Solution);
+  console.log("Part 1 Solution:", part1Solution, "\nPart 2 Solution:", part2Solution);
+})();
